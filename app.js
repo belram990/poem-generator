b54e1cd46215b8a813f44a450deea97fc26aabab
Donabel <157907649+belram990@users.noreply.github.com>
2024-12-29 15:25:09 -0500

    updated css and js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function generatePoem(event) {
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class="generating">Generating a poem about ${instructionsInput.value} ....</div>`;
+  poemElement.innerHTML = `<div class="generating">Generating a poem about ${instructionsInput.value}....</div>`;
 
   console.log("Generate poem");
   console.log(`Prompt: ${prompt}`);
